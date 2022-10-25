04e3cc273fa7c2161fe4b1e1a01e610147b172d8
h.safari <h.safari@roundtableapps.com>
2022-10-25 23:33:25 +0330

    add user & role migrations

--- db/migrate/20221025193915_create_user_roles.rb
+++ db/migrate/20221025193915_create_user_roles.rb
@@ -1,6 +1,8 @@
 class CreateUserRoles < ActiveRecord::Migration[6.1]
   def change
     create_table :user_roles do |t|
+      t.references :user, foreign_key: true
+      t.references :role, foreign_key: true
 
       t.timestamps
     end
