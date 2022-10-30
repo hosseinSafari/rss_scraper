50b494f65d11c3e18ff3685350bd5e38991d9203
h.safari <h.safari@roundtableapps.com>
2022-10-30 21:47:01 +0330

    add some feeds_controller test

--- spec/requests/api/v1/feeds_spec.rb
+++ spec/requests/api/v1/feeds_spec.rb
@@ -1,6 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Feeds", type: :request do
-  describe "Login Action" do
+  describe "Index Action" do
+    before(:each) do
+      sign_in_user
+    end
+
+    it "Unauthorized" do
+
+      get "/api/v1/feeds"
+      expect(response).to have_http_status(401)
+    end
+
+    it "Success" do
+      get "/api/v1/feeds",
+      headers: {Authorization: @bearer_token}
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "Show Action" do
+    before(:each) do
+      sign_in_user
+      @new_site = ::Site.create(url: "site_url.com")
+      @new_feed = ::Feed.create(title: "title1",
+                                url: "feed_url.com",
+                                description: "It's a description.",
+                                site_id: @new_site&.id)
+    end
+
+    it "Unauthorized" do
+
+      get "/api/v1/feeds/#{@new_feed&.id}"
+      expect(response).to have_http_status(401)
+    end
+
+    it "BadRequest --> the current user dosn't register for the above site." do
+      get "/api/v1/feeds/#{@new_feed&.id}",
+      headers: {Authorization: @bearer_token}
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it "BadRequest --> feed not found." do
+      get "/api/v1/feeds/#{-3}",
+      headers: {Authorization: @bearer_token}
+      expect(response).to have_http_status(:bad_request)
+    end
+
+
+    it "Success" do
+      @new_user_site = ::UserSite.create(user: @current_user, site: @new_site)
+      get "/api/v1/feeds/#{@new_feed&.id}",
+      headers: {Authorization: @bearer_token}
+      expect(response).to have_http_status(:bad_request)
+    end
   end
 end
--- spec/requests/api/v1/websites_spec.rb
+++ spec/requests/api/v1/websites_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Websites", type: :request do
-  describe "Login Action" do
+  describe "AddUrl Action" do
     before(:each) do
       sign_in_user
     end
@@ -12,6 +12,19 @@ RSpec.describe "Api::V1::Websites", type: :request do
       headers: {Authorization: @bearer_token}
       
       expect(response).to have_http_status(:ok)
+    end
+
+    it "Unauthorized" do
+      post "/api/v1/websites/add_url"
+      
+      expect(response).to have_http_status(401)
+    end
+
+    it "Bad request" do
+      post "/api/v1/websites/add_url", 
+      headers: {Authorization: @bearer_token}
+      
+      expect(response).to have_http_status(:bad_request)
     end
   end
 end
